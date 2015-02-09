6d075d3fa1dc5ea48b2be25be1ae77fa4f3a75da
fujohnwang <fujohnwang@gmail.com>
2015-02-09 17:41:15 +0800

    update data table sample code

--- app/controllers/DataTable.scala
+++ app/controllers/DataTable.scala
@@ -8,9 +8,25 @@ import vo.JsonTest.Row
 @Component
 class DataTableController extends Controller {
   def list() = Action { implicit request =>
-    val row1 = new Row("darren", 35)
-    val row2 = new Row("darren", 35)
-    val dt = DataTableView(1, 1, Seq(row1, row2))
+    //    val row1 = new Row("darren", 35)
+
+    val draw: Int = request.getQueryString("draw") match {
+      case Some(d) => d.toInt
+      case None => 1
+    }
+
+    val start: Int = request.getQueryString("start") match {
+      case Some(page) => page.toInt
+      case None => 0
+    }
+
+    val pageSize = request.getQueryString("length").get.toInt
+
+    //    val query = request.getQueryString("search[value]")
+    //    println(query.get)
+
+    val rows = (0 until 100).map(_ => new Row("darren", 35))
+    val dt = DataTableView(draw, rows.size, rows.slice(start, start + pageSize))
     Ok(dt.asJson())
   }
 }
--- public/js/datatable.js
+++ public/js/datatable.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $('#example').dataTable({
         "processing": true,
         "serverSide": true,
+        searching: false,
+        ordering:  false,
         "ajax": '/data'
     });
 });
